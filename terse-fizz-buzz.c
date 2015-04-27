c97cd2b125a23fd4c276e625f84e0765b1487f94
Tony Finch <dot@dotat.at>
2015-04-27 19:56:28 +0100

    Briefer

--- terse-fizz-buzz.c
+++ terse-fizz-buzz.c
@@ -2,9 +2,9 @@
 #include <err.h>
 
 int main(void) {
-  for (int n, i = 0;; i++)
-    printf("%s%s%n",
-	   i % 3 == 0 ? "Fizz" : "",
-	   i % 5 == 0 ? "Buzz" : "",
-	   &n), printf(n ? "\n" : "%i\n", i);
+  for (int i = 0;; i++)
+    printf(printf("%s%s"
+      , i % 3 ? "" : "Fizz"
+      , i % 5 ? "" : "Buzz"
+      ) ? "\n" : "%i\n", i);
 }
